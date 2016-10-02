fbe51d1f6529d21ac0c78fd4b49472cfa8e4851c
keremcaliskan7 <keremcaliskan@outlook.com>
2016-10-02 12:35:25 +0300

    Changed Free Open Source to Our Projects. Improves #4
    Changed title and content of "Free & Open Source".

--- templates/business-simple/index.php
+++ templates/business-simple/index.php
@@ -25,10 +25,10 @@
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-gift"></i> Free &amp; Open Source</h4>
+                        <h4><i class="fa fa-fw fa-gift"></i> Our Projects</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <p>We are making a project for Software Engineering lecture at Anadolu University. We are sharing ideas about project and making plans. </p>
                         <a href="#" class="btn btn-default">Learn More</a>
                     </div>
                 </div>
