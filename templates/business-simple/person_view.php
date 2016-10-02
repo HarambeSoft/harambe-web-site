0edfe24f3f425c5c22c01c13c5074109f812e806
İsa Mert Gürbüz <isamertgurbuz@gmail.com>
2016-10-02 14:03:55 +0300

    Fix <br> usage

--- templates/business-simple/person_view.php
+++ templates/business-simple/person_view.php
@@ -2,7 +2,7 @@
 <div class="well text-center fixed-panel">           
     <img src="<?php echo $person->image(); ?>" class="img-circle" alt="<?php echo $person->name(); ?>" width="175" height="175" /> 
     <h2><?php echo $person->name(); ?></h2>
-    <p><?php echo $person->info(); ?>
-    <br><?php echo $person->email(); ?></p>
+    <p><?php echo $person->info(); ?></p>
+    <p><?php echo $person->email(); ?></p>
 </div>
 </div>
