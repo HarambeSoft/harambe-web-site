786c4b9612fabc8a254ed092ba3482d7db384cd1
keremcaliskan7 <keremcaliskan@outlook.com>
2016-10-02 12:31:17 +0300

    Changed text Bootstrap to Our Goal. Improves #4

--- templates/business-simple/index.php
+++ templates/business-simple/index.php
@@ -14,10 +14,10 @@
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.3.7</h4>
+                        <h4><i class="fa fa-fw fa-check"></i> Our Goal</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <p>Our goal is to develop lightweight and useful mobile applications. We are working in colloboration with our team of nine engineers. For now we are developing for Android devices. </p>
                         <a href="#" class="btn btn-default">Learn More</a>
                     </div>
                 </div>
