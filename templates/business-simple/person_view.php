08c277564150f20f37d59cf9a346ddb8fbd40170
alperenkirimer <alperenkirimer@gmail.com>
2016-10-02 14:08:40 +0300

    mail fix

--- templates/business-simple/person_view.php
+++ templates/business-simple/person_view.php
@@ -3,6 +3,6 @@
     <img src="<?php echo $person->image(); ?>" class="img-circle" alt="<?php echo $person->name(); ?>" width="175" height="175" /> 
     <h2><?php echo $person->name(); ?></h2>
     <p><?php echo $person->info(); ?></p>
-    <p><?php echo $person->email(); ?></p>
+    <a href="mailto:<?php echo $person->email(); ?>">e-mail: <?php echo $person->email(); ?></a>
 </div>
 </div>
