e9090714d7becc679c026ff713fb43ab640ae315
alperenkirimer <alperenkirimer@gmail.com>
2016-10-03 09:58:47 +0300

    Update header.php

--- templates/business-simple/header.php
+++ templates/business-simple/header.php
@@ -86,7 +86,7 @@
             <div class="item">
                 <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
                 <div class="carousel-caption">
-                    <h2> </h2>
+                    <h2>We make apps for living</h2>
                 </div>
             </div>
         </div>
