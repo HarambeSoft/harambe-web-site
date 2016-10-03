985c652569011dbe95fa524bbbf01d5f6dc36f4a
alperenkirimer <alperenkirimer@gmail.com>
2016-10-03 09:54:28 +0300

    footer contact change

--- templates/business-simple/footer.php
+++ templates/business-simple/footer.php
@@ -2,7 +2,9 @@
         <div id="contact" class="well">
             <div class="row">
                 <div class="col-md-8">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+                    <p> 
+                        <a href="https://github.com/HarambeSoft/">GitHub</a>
+                    </p>
                 </div>
                 <div class="col-md-4">
                     <a class="btn btn-lg btn-default btn-block" href="#">Call to Action</a>
