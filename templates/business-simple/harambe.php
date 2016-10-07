a55a7d0253c6a7faea74398b89edce0a0c9a576c
isamert <isamertgurbuz@gmail.com>
2016-10-07 11:22:34 +0300

    Updates #10

--- templates/business-simple/harambe.php
+++ templates/business-simple/harambe.php
@@ -8,12 +8,12 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Harambe
-                <small>, the heroic gorilla</small>
+                <small>the heroic gorilla</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="index.html">Home</a>
                 </li>
-                <li class="active">Full Width Page</li>
+                <li class="active">About Harambe</li>
             </ol>
         </div>
     </div>
@@ -22,15 +22,15 @@
     <!-- Content Row -->
     <div class="row">
         <div class="col-lg-12">
+            <img src="misc/img/harambe_with_boy.jpg" alt="Harambe with boy" class="pull-right">
             <p>It was only one day after his 17th birthday. 
                 Harambe was killed right after a 3 year old child fell into his habitat in Cincinnati Zoo. 
                 Although Harambe did not do any harm to the boy and carried him to a safer place, zoo officials were agreed upon killing Harambe.
                 A single gunshot was fired.
                 Maybe it was the right decision, however he should not have been put in a cage in the first place.
                 Animals should be free, and so are the softwares.
-                <br>
-                As long as he is remembered, he will not really die at all.
-                <small><a href="https://en.wikipedia.org/wiki/Killing_of_Harambe"> Further reading</a></small>
+                <p>As long as he is remembered, he will not really die at all.</p>
+                <p><small><a href="https://en.wikipedia.org/wiki/Killing_of_Harambe"> Further reading</a></small></p>
             </p>
         </div>
     </div>
