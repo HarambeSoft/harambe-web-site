bd0621fef163074f94d567ee3867b5c75e31e6d6
alperenkirimer <alperenkirimer@gmail.com>
2016-10-07 09:55:27 +0300

    harambe text

--- templates/business-simple/harambe.php
+++ templates/business-simple/harambe.php
@@ -7,8 +7,8 @@
     <!-- Page Heading/Breadcrumbs -->
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Full Width Page
-                <small>Subheading</small>
+            <h1 class="page-header">Harambe
+                <small>, the heroic gorilla</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="index.html">Home</a>
@@ -22,7 +22,14 @@
     <!-- Content Row -->
     <div class="row">
         <div class="col-lg-12">
-            <p>Some content.</p>
+            <p>It was only one day after his 17th birthday. 
+                Harambe was killed right after a 3 year old child fell into his habitat in Cincinnati Zoo. 
+                Although Harambe did not do any harm to the boy and carried him to a safer place, zoo officials were agreed upon killing Harambe.
+                A single gunshot was fired.
+                Maybe it was the right decision, however he should not have been put in a cage in the first place.
+                Animals should be free, and so are the softwares.
+                <br>
+                As long as he is remembered, he will not really die at all.</p>
         </div>
     </div>
     <!-- /.row -->
